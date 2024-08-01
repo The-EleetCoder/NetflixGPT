f1251479b20593bf76258b1bb00f89dd5135383a
The-EleetCoder <jaijain1803@gmail.com>
2024-08-02 00:12:19 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      YOLO
-    </div>
+    <h1 className="text-6xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
