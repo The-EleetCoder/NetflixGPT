e4d46622fc42532950d33792c09b29db07fb44d1
The-EleetCoder <jaijain1803@gmail.com>
2024-08-02 23:58:32 +0530

    login form css

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
       {/* form */}
       <form className="w-3/12 h-fit min-w-[25rem] p-12 bg-[#111111d8] self-center mx-auto flex flex-col rounded-md">
         {/* sign in heading */}
-        <div className="text-white font-bold text-3xl mb-[1.5rem]">Sign In</div>
+        <div className="text-white font-bold text-3xl mb-[2rem]">Sign In</div>
 
         {/* input */}
         <input
@@ -24,7 +24,7 @@ const Login = () => {
         />
 
         {/* sign in button */}
-        <button className=" bg-[#D9232E] p-2 my-2 mt-[1.5rem] rounded-sm">
+        <button className=" bg-[#D9232E] p-2 my-2 mt-[2rem] rounded-sm">
           Sign in
         </button>
 
@@ -32,6 +32,18 @@ const Login = () => {
         <div className="flex items-center gap-1 text-sm h-[1rem]">
           <input type="checkbox" className="" />
           <label className="mb-1">Remember me</label>
+        </div>
+
+        {/* footer */}
+        <div className="mt-10">
+          <div className="">
+            New to Netflix?{" "}
+            <span className="font-semibold text-white">Sign up now.</span>
+          </div>
+          <div className="text-[13px] mt-4">
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <span className=" text-blue-500">Learn more.</span>
+          </div>
         </div>
       </form>
     </div>
