0aeb749ddca1ea24debbc95ac6020cd0f58eddea
The-EleetCoder <jaijain1803@gmail.com>
2024-08-02 20:43:04 +0530

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,15 +3,37 @@ import Header from "./Header";
 
 const Login = () => {
   return (
-    <div>
+    <div className="bg-custom-background w-full min-h-[100vh] h-full flex text-[#ffffffc6]">
       <Header />
-      <div className="w-full min-h-[100vh] bg-slate-500">
-        <img
-        className="w-full min-h-[100vh] h-full"
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/826348c2-cdcb-42a0-bc11-a788478ba5a2/6d20b198-e7ab-4e9f-a1aa-666faa0298f9/IN-en-20240729-POP_SIGNUP_TWO_WEEKS-perspective_WEB_a67d8c9e-8121-4a74-98e4-8005eb2df227_large.jpg"
-          alt="backgroung-image"
+
+      {/* form */}
+      <form className="w-3/12 h-fit min-w-[25rem] p-12 bg-[#111111d8] self-center mx-auto flex flex-col rounded-md">
+        {/* sign in heading */}
+        <div className="text-white font-bold text-3xl mb-[1.5rem]">Sign In</div>
+
+        {/* input */}
+        <input
+          type="text"
+          placeholder="email address"
+          className="p-2 px-4 my-2 w-full bg-[#121212] border-[#3e3e3e] border-[1px] focus:border-[#D9232E] focus:outline-none rounded-sm"
         />
-      </div>
+        <input
+          type="password"
+          placeholder="password"
+          className="p-2 px-4 my-2 w-full bg-[#121212] border-[#3e3e3e] border-[1px] focus:border-[#D9232E] focus:outline-none rounded-sm"
+        />
+
+        {/* sign in button */}
+        <button className=" bg-[#D9232E] p-2 my-2 mt-[1.5rem] rounded-sm">
+          Sign in
+        </button>
+
+        {/* Remember me */}
+        <div className="flex items-center gap-1 text-sm h-[1rem]">
+          <input type="checkbox" className="" />
+          <label className="mb-1">Remember me</label>
+        </div>
+      </form>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-background":
+          "url('https://assets.nflxext.com/ffe/siteui/vlv3/826348c2-cdcb-42a0-bc11-a788478ba5a2/6d20b198-e7ab-4e9f-a1aa-666faa0298f9/IN-en-20240729-POP_SIGNUP_TWO_WEEKS-perspective_WEB_a67d8c9e-8121-4a74-98e4-8005eb2df227_large.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
