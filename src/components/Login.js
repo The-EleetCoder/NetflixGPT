7abb78a3c395dd8535fdfce346e460a916ec9983
The-EleetCoder <jaijain1803@gmail.com>
2024-08-03 18:05:14 +0530

    Used Formik and Yup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,27 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import Header from "./Header";
 
 const Login = () => {
   const [isLoginForm, setIsLoginForm] = useState(true);
 
-  const username = useRef(null);
-  const email = useRef(null);
-  const password = useRef(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username?.current?.value);
-    console.log(email?.current?.value);
-    console.log(password?.current?.value);
-  };
-
   const handleClick = () => {
     setIsLoginForm(!isLoginForm);
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: !isLoginForm
+      ? Yup.string().required("Username is required")
+      : null,
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
   };
 
   return (
@@ -26,66 +31,111 @@ const Login = () => {
         <Header />
 
         {/* form */}
-        <form
+        <Formik
+          initialValues={{ username: "", email: "", password: "" }}
+          validationSchema={validationSchema}
           onSubmit={handleSubmit}
-          className="w-3/12 h-fit min-w-[25rem] p-12 bg-[#111111da] self-center mx-auto flex flex-col rounded-md"
         >
-          {/* sign in heading */}
-          <div className="text-white font-bold text-3xl mb-[2rem]">
-            {isLoginForm ? "Sign In" : "Sign Up"}
-          </div>
+          {({ errors, touched }) => (
+            <Form className="w-3/12 h-fit min-w-[25rem] p-12 bg-[#111111da] self-center mx-auto flex flex-col rounded-md">
+              {/* sign in heading */}
+              <div className="text-white font-bold text-3xl mb-[2rem]">
+                {isLoginForm ? "Sign In" : "Sign Up"}
+              </div>
 
-          {/* input */}
-          {!isLoginForm && (
-            <input
-              ref={username}
-              type="text"
-              placeholder="Username"
-              className="p-2 px-4 my-2 w-full bg-[#121212] border-[#4c4c4c] border-[1px] focus:border-[#D9232E] focus:outline-none rounded-sm"
-            />
-          )}
-          <input
-            ref={email}
-            type="text"
-            placeholder="Email"
-            className="p-2 px-4 my-2 w-full bg-[#121212] border-[#4c4c4c] border-[1px] focus:border-[#D9232E] focus:outline-none rounded-sm"
-          />
-          <input
-            ref={password}
-            type="password"
-            placeholder="Password"
-            className="p-2 px-4 my-2 w-full bg-[#121212] border-[#4c4c4c] border-[1px] focus:border-[#D9232E] focus:outline-none rounded-sm"
-          />
+              {/* input */}
+              {!isLoginForm && (
+                <div>
+                  <Field
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    className={`p-2 px-4 my-2 w-full bg-[#121212] border-[#4c4c4c] border-[1px] focus:border-[#a0a0a0] focus:outline-none rounded-sm ${
+                      errors.username && touched.username
+                        ? "border-[#D9232E]"
+                        : ""
+                    }`}
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="text-[#D9232E] text-sm mt-[-0.4rem] mb-2 text-right"
+                  />
+                </div>
+              )}
+              <div>
+                <Field
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                  className={`p-2 px-4 my-2 w-full bg-[#121212] border-[#4c4c4c] border-[1px] focus:border-[#a0a0a0] focus:outline-none rounded-sm ${
+                    errors.email && touched.email ? "border-[#D9232E]" : ""
+                  }`}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-[#D9232E] text-sm mt-[-0.4rem] mb-2 text-right"
+                />
+              </div>
+              <div>
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  className={`p-2 px-4 my-2 w-full bg-[#121212] border-[#4c4c4c] border-[1px] focus:border-[#a0a0a0] focus:outline-none rounded-sm ${
+                    errors.password && touched.password
+                      ? "border-[#D9232E]"
+                      : ""
+                  }`}
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-[#D9232E] text-sm mt-[-0.4rem] mb-2 text-right"
+                />
+              </div>
 
-          {/* sign in button */}
-          <button className=" bg-[#D9232E] p-2 my-2 mt-[2rem] rounded-sm">
-            {isLoginForm ? "Sign In" : "Sign Up"}
-          </button>
-
-          {/* Remember me */}
-          <div className="flex items-center gap-1 text-sm h-[1rem]">
-            <input type="checkbox" className="cursor-pointer" />
-            <label className="mb-1">Remember me</label>
-          </div>
-
-          {/* footer */}
-          <div className="mt-10">
-            <div className="">
-              {isLoginForm ? "New to Netflix?" : "Already a user?"}
-              <span
-                onClick={handleClick}
-                className="font-semibold text-white cursor-pointer"
+              {/* sign in button */}
+              <button
+                className=" bg-[#D9232E] p-2 my-2 mt-[2rem] rounded-sm"
+                type="submit"
               >
-                {isLoginForm ? " Sign Up now." : " Sign in now."}
-              </span>
-            </div>
-            <div className="text-[13px] mt-4">
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.{" "}
-              <span className=" text-blue-500 cursor-pointer">Learn more.</span>
-            </div>
-          </div>
-        </form>
+                {isLoginForm ? "Sign In" : "Sign Up"}
+              </button>
+
+              {/* Remember me */}
+              <div className="flex items-center gap-1 text-sm h-[1rem]">
+                <Field
+                  type="checkbox"
+                  name="rememberMe"
+                  className="cursor-pointer"
+                />
+                <label className="mb-1">Remember me</label>
+              </div>
+
+              {/* footer */}
+              <div className="mt-10">
+                <div className="">
+                  {isLoginForm ? "New to Netflix?" : "Already a user?"}
+                  <span
+                    onClick={handleClick}
+                    className="font-semibold text-white cursor-pointer"
+                  >
+                    {isLoginForm ? " Sign Up now." : " Sign in now."}
+                  </span>
+                </div>
+                <div className="text-[13px] mt-4">
+                  This page is protected by Google reCAPTCHA to ensure you're
+                  not a bot.{" "}
+                  <span className=" text-blue-500 cursor-pointer">
+                    Learn more.
+                  </span>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
